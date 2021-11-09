66adf3517ef5525e39dba76c3dbdc3cedcad5f67
Reza Qavi <rezaqavi1378@gmail.com>
2021-11-09 07:50:04 +0330

    Compelete MEM define signiture

--- MEM_Reg.v
+++ MEM_Reg.v
@@ -1,20 +1,25 @@
 module MEM_Reg (
-    input clk, rst, freeze, flush,
-    input[31:0] PC_in, instruction_in,
-    output reg[31:0] PC, instruction
+    input clk, rst, WB_EN_MEM, MEM_R_EN_MEM,
+    input[31:0] ALU_res_MEM, mem_out_MEM,
+    input[3:0] Dest_MEM, 
+
+    output reg WB_EN_WB, MEM_R_EN_WB, 
+    output reg[31:0] ALU_res_WB, mem_out_WB,
+    output reg[3:0] Dest_WB
+
 );
 
-    always @(posedge clk, posedge rst) begin
-        if (rst) begin
-            PC <= 0;
-            instruction <= 0;
-        end else if (flush) begin
-            PC <= 0;
-            instruction <= 0;
-        end else if (~freeze) begin
-            PC <= PC_in;
-            instruction <= instruction_in;
-        end
-    end
+    // always @(posedge clk, posedge rst) begin
+    //     if (rst) begin
+    //         PC <= 0;
+    //         instruction <= 0;
+    //     end else if (flush) begin
+    //         PC <= 0;
+    //         instruction <= 0;
+    //     end else if (~freeze) begin
+    //         PC <= PC_in;
+    //         instruction <= instruction_in;
+    //     end
+    // end
 
 endmodule
